6ac151cde29856fd03c335e7bf3911f7b2f3cbe5
Julio Wandekoken <jwandekoken@gmail.com>
2022-04-19 07:08:41 -0300

    chore: scaffolded post

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -9,10 +9,17 @@
 import BlogLayout from 'src/layouts/BlogLayout'
 
 import { Router, Route, Set } from '@redwoodjs/router'
+import PostsLayout from 'src/layouts/PostsLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Set wrap={PostsLayout}>
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
+      </Set>
       <Set wrap={BlogLayout}>
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
