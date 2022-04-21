c8d5e883d9377f60834831f41761c4ea1ad25e6a
Julio Wandekoken <jwandekoken@gmail.com>
2022-04-21 11:47:26 -0300

    chore: advancing saving data tutorial section

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -17,7 +17,7 @@ const ContactPage = () => {
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
         <Label name="name" errorClassName="error">
           Name
         </Label>
@@ -33,7 +33,13 @@ const ContactPage = () => {
         </Label>
         <TextField
           name="email"
-          validation={{ required: true }}
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
           errorClassName="error"
         />
         <FieldError name="email" className="error" />
